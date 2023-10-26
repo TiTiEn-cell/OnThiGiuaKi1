78f5e5320ec33801cddd2d6b6a943e8102590e2a
Tan Tai <ntantai123321@gmail.com>
2023-10-26 21:22:14 +0700

    complete UI Start

--- App.js
+++ App.js
@@ -3,13 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import StartScreen from './Component/StartScreen';
 import ListScreen from './Component/ListScreen';
 import DetailScreen from './Component/DetailScreen';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="StartScreen" component={StartScreen} />
+        <Stack.Screen name="ListScreen" component={ListScreen} />
+        <Stack.Screen name="DetailScreen" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- Component/StartScreen.js
+++ Component/StartScreen.js
@@ -1,10 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
 
 export default function StartScreen() {
   return (
     <View style={styles.container}>
-      
+      <Text style ={{fontSize: 26, fontWeight: 400}}>A premium online store for sporter and their stylish choice</Text>
+        <View style ={{
+            backgroundColor:'#E941411A',
+            borderRadius: 50,
+            width: 359,
+            height: 388,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 20,
+        }}>
+        <Image 
+        source={require('../Image/bifour_-removebg-preview.png')}
+        style = {{width:292, 
+            height: 270, 
+            
+            
+        }}
+        />
+        
+        </View>
+        <Text style = {{
+            fontSize: 26,
+            fontStyle: 'Bold',
+            //fontWeight: 700,
+        }}>POWER BIKE SHOP</Text>
+
+        <TouchableOpacity style = {{
+            borderRadius: 30,
+            backgroundColor: '#E94141',
+            width: 340,
+            height: 61,
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <Text style = {{
+                fontSize: 27, 
+                fontWeight: 400,
+            }}>Get Started</Text>
+        </TouchableOpacity>
+        
     </View>
   );
 }
@@ -12,8 +52,6 @@ export default function StartScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
